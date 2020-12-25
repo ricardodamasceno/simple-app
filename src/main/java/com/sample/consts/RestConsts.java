d2e5c9cbe898e2d6ff795501a616d709ab69ec9f
Ricardo Damasceno <ricardodamascenno@gmail.com>
2020-12-25 14:55:32 -0300

    Creation of home controller

---
+++ src/main/java/com/sample/consts/RestConsts.java
@@ -0,0 +1,8 @@
+package com.sample.consts;
+
+public class RestConsts {
+
+    public static final String V1 = "/rest/v1";
+    public static final String HOME = "/home";
+
+}
---
+++ src/main/java/com/sample/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.sample.controller;
+
+import com.sample.consts.RestConsts;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = RestConsts.V1 + RestConsts.HOME)
+public class HomeController {
+
+    @GetMapping
+    public @ResponseBody String home(){
+        return "App home is responding !!! ";
+    }
+
+}
